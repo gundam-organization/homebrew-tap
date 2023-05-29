c4e2f2507702b5cde125d04eccb6cf105277b5d1
Adrien Blanchet <adrien.blanchet@live.fr>
2023-05-29 20:07:32 +0200

    removing bottle ref

--- gundam.rb
+++ gundam.rb
@@ -11,10 +11,10 @@ class Gundam < Formula
   license "LGPL-2.1 license"
   head "https://github.com/gundam-organization/gundam.git", branch: "main"
 
-  bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_ventura: "22a95a3ead8a4a6fb45149abe6f8162cffffd08ba12c84fa30234b9d7c7f4cc6"
-  end
+  # bottle do
+  #   rebuild 1
+  #   sha256 cellar: :any, arm64_ventura: "22a95a3ead8a4a6fb45149abe6f8162cffffd08ba12c84fa30234b9d7c7f4cc6"
+  # end
 
   # depends_on "cmake" => :build
 
