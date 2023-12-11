cc0b207c202e470da5f8d2ac2f852ec9aa822acf
Adrien Blanchet <adrien.blanchet@live.fr>
2023-12-11 21:35:58 +0100

    Formula/gundam.rb

--- Formula/gundam.rb
+++ Formula/gundam.rb
@@ -4,8 +4,8 @@ class Gundam < Formula
   desc "Statistical analysis tool that allows to host various studies with config files"
   homepage "https://github.com/gundam-organization/gundam"
   url "https://github.com/gundam-organization/gundam.git",
-    tag:      "1.7.1",
-    revision: "cbcbf2a16a844294cf6acf5721b26d1fa6dc61e5"
+    tag:      "1.8.0",
+    revision: "c799c10017b51bbfca88c8216a94dfb8eaca4558"
   license "LGPL-2.1-or-later"
   head "https://github.com/gundam-organization/gundam.git", branch: "main"
 
@@ -21,6 +21,7 @@ class Gundam < Formula
       -D USE_STATIC_LINKS=ON
     ]
 
+    ENV["ROOTSYS"]="$(brew --prefix)"
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
