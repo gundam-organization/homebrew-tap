e0274ac45206b8a22a7104d0c8b2cc79019cd58b
Adrien Blanchet <adrien.blanchet@live.fr>
2026-06-30 10:13:43 +0200

    Modernize bottle workflows and bump gundam to 1.8.9

--- Formula/gundam.rb
+++ Formula/gundam.rb
@@ -1,29 +1,32 @@
-# Ruby script that describes how GUNDAM should be installed on your computer
-
 class Gundam < Formula
   desc "Statistical analysis tool that allows to host various studies with config files"
   homepage "https://github.com/gundam-organization/gundam"
   url "https://github.com/gundam-organization/gundam.git",
-    tag:      "1.8.0",
-    revision: "c799c10017b51bbfca88c8216a94dfb8eaca4558"
+    tag:      "1.8.9",
+    revision: "09305abff5a7c09dc783b6f524a3529112cfce75"
   license "LGPL-2.1-or-later"
   head "https://github.com/gundam-organization/gundam.git", branch: "main"
 
   depends_on "cmake" => :build
   depends_on "nlohmann-json" => :build
-  depends_on "yaml-cpp" => :build
-  depends_on "zlib" => :build
   depends_on "root"
+  depends_on "yaml-cpp"
+  depends_on "zlib"
 
   def install
-    args = std_cmake_args + %w[
+    ENV["ROOTSYS"] = formula_opt_prefix("root")
+
+    args = %w[
       -D WITH_GUNDAM_ROOT_APP=OFF
       -D USE_STATIC_LINKS=ON
     ]
 
-    ENV["ROOTSYS"]="$(brew --prefix)"
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/gundamFitter 2>&1", 1)
   end
 end
